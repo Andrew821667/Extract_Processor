7bdaba49dccf709aa6f2881b8469a18f5c6f2bcb
Andrew821667 <a.popov.gv@gmail.com>
2025-06-11 07:23:59 +0000

    Удаление временных файлов после оптимизации

--- quick_fix.py
+++
@@ -1,4 +0,0 @@
-# Quick fix for auto_process_with_detection method
-def auto_process_with_detection_fixed(self, file_path):
-    '''Исправленная версия автоматической обработки'''
-    return self.process_single_file_advanced(file_path)
